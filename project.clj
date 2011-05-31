b161deb9e63da2475b4e367aea93fa4ec9ee38ad
Cory Giles <cory.b.giles@gmail.com>
2011-05-31 13:07:50 -0500

    Fixed dependencies to use all JARs on Clojars

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject nlptk "1.0.0-SNAPSHOT"
   :description "A collection of basic NLP utilities."
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [com.aliasi/lingpipe "4.0.1"]
-                 [edu.stanford.nlp/stanford-parser "1.6.5"]
+                 [org.clojars.gilesc/stanford-parser "1.6.7"]
                  [edu.sinica.bioagent/bioadi "0.1.0"]])
