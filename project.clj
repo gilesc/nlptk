8ca3b69c00a27052d77129723c877e2bad3835ea
Cory Giles <cory.b.giles@gmail.com>
2011-06-21 15:17:48 -0500

    Parser for SORD tblObjectSynonyms

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject es.corygil/nlptk "1.0.0-SNAPSHOT"
-  :description "A collection of basic NLP utilities."
+  :description "NLP utilities (NER, parsers, corpus readers)"
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [com.aliasi/lingpipe "4.0.1"]
                  [org.clojars.gilesc/stanford-parser "1.6.7"]
-                 [edu.sinica.bioagent/bioadi "0.1.0"]]
+                 [edu.sinica.bioagent/bioadi "0.1.0"]
+                 [com.healthmarketscience/jackcess "1.2.4"]]
   :source-path "src/clj"
   :java-source-path "src/java")
--- src/clj/es/corygil/nlptk/corpus.clj
+++ src/clj/es/corygil/nlptk/corpus.clj
@@ -1,8 +1,9 @@
 (ns es.corygil.nlptk.corpus
   (:require
-   [clojure.contrib.io :as io]))
+   [clojure.contrib.io :as io]
+   [clojure.contrib.seq :as seq]))
 
-(defn to-input-strm [file]
+(defn- to-input-strm [file]
   (if (.endsWith (str file) "gz")
     (java.util.zip.GZIPInputStream.
      (io/input-stream file))
@@ -14,3 +15,26 @@
     {:pmid (.pmid citation)
      :title (.title citation)
      :abstract (.abstrct citation)}))
+
+(defn read-sord-terms [sord-file]
+  "Read Named Entities from SORD (streamlined object relational database)"
+  (for [[id records] (group-by :RecordID
+                               (map #(zipmap (map keyword (keys %)) (vals %))
+                                    (iterator-seq
+                                     (.iterator
+                                      (.getTable
+                                       (com.healthmarketscience.jackcess.Database/open
+                                        (io/file sord-file))
+                                       "tblObjectSynonyms")))))]
+    (merge
+     {:id id :name (:Objectname (first records))
+      :ontology (ffirst
+                 (reverse
+                  (sort-by second
+                           (frequencies (map :ObjectType records)))))
+      :sources (set (map :SourceID records))
+      :birth-date (apply min (map :Birth_Date records))
+      :frequency (apply max (map :Occurances records))}
+     (zipmap [:synonyms-ci :synonyms]
+             (map #(map :Objectsynonym %)
+                    (seq/separate :CAPS_flag records))))))
